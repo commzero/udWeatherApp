9cae149e923bb1aa3aad40b4afc51d4dbdb8d4f6
lobna <mwahshey@seamlabs.com>
2021-02-14 20:48:38 +0200

    created server js from scratch

--- server.js
+++ server.js
@@ -2,18 +2,40 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
-
+const app = express();
+const bodyParse = require('body-parser');
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParse.urlencoded({ extended: false }));
+app.use(bodyParse.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 3000;
+const server = app.listen(port, listening);
+function listening() {
+    console.log('server running');
+    console.log(`running on localhost: ${port}`);
+};
+// POST route
+app.post('/', (req, res) => {
+    projectData.date = req.body.date;
+    projectData.temperature = req.body.main.temp;
+    projectData.feelings = req.body.feelings;
+    console.log('POST request received');
+    res.end();
+});
+
+// GET route
+app.get('/data', (req, res) => {
+    console.log('GET request received');
+    res.send(projectData);
+});
